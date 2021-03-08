a9fed7f132c65ab87a1a55678cce2ae20aca9847
Damislav <idamjanovic28@gmail.com>
2021-03-08 14:58:06 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import Error from "./pages/Error";
+import SingleRoom from "./pages/SingleRoom";
+import { Route, Switch } from "react-router-dom";
+
 export default class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+      </>
+    );
   }
 }
