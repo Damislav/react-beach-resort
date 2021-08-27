091f61093147348ee6e2a367d48ee4f28bf959ae
Damislav <damislav@net.hr>
2021-08-27 22:43:20 +0200

    second

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import defaultBcg from "../images/room-1.jpeg";
-import Hero from "../components/Main/Hero";
+// import Hero from "../components/Main/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../context/context";
